2056960fe0557b2a5ea8700ff91ff43eec1e6deb
Unraelity <nicholasjonathonbrown@gmail.com>
2025-12-04 15:49:01 -0700

    Removed URI

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 // to use mongoDB
 const mongoose = require("mongoose");
-require('dotenv').config();
-
 
 mongoose.set('strictQuery', true);
-
-const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1/authen";
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology:true });
+mongoose.connect("mongodb://127.0.0.1/authen", { useNewUrlParser: true, useUnifiedTopology:true });
 
 module.exports = mongoose;
