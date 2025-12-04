b6d55043ebf20e2c00e3428469d3ae29755cbf26
Unraelity <nicholasjonathonbrown@gmail.com>
2025-12-04 15:02:15 -0700

    Added .env file with api key

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 // to use mongoDB
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://127.0.0.1/authen", { useNewUrlParser: true, useUnifiedTopology:true });
+
+const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1/authen";
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology:true });
 
 module.exports = mongoose;
